54a3774d88ae95c760863647ad35b588569803df
Tuic242 <jeremy242mou@gmail.com>
2019-02-26 11:15:56 +0100

    Create main.js

---
+++ .gitignore/main.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const express = require('express');
+const app = express();
+
+
+//Debut Parametres Heroku 
+app.set('port',(process.env.PORT || 5000))
+
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: '[Help.exe] Bot By Tuic242', type: 0}});
+    console.log("Bot Ready !");
+})
+
+
+bot.on('message', function (message) {
+    if (message.content === 'Help.exe') {
+        message.channel.send('**Bot pas encore code**')
+    }
+})
+
+bot.login(process.env.TOKEN)
+
